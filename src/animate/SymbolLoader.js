5561149221998b5dbf9c4f82b805883327597fbb
MadCreativity <ahlgreenmadsen@gmail.com>
2021-02-22 15:19:52 +0100

    Remove unnecessary texture caching.

--- src/animate/SymbolLoader.js
+++ src/animate/SymbolLoader.js
@@ -19,10 +19,10 @@ let SymbolLoader = {
         } else if (data.nodeName && data.nodeName === 'IMG') {
             // Add individual images to the texture cache by their
             // short symbol name, not the URL
-            PIXI.Texture.addToCache(
+            /*PIXI.Texture.addToCache(
                 resource.texture,
                 resource.name
-            );
+            );*/
         }
         next();
     }
